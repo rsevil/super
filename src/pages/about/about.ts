771a4a2cdbcb4b6d653ea0ec803b900387345ab4
rsevil <7DeMarzo2009>
2018-03-02 17:22:00 -0300

    born

---
+++ src/pages/about/about.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+
+  constructor(public navCtrl: NavController) {
+    this.bind();
+  }
+
+  /**
+   * name
+   */
+  public bind() {
+    console.log('bind');
+  }
+
+}
